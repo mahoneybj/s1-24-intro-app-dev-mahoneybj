aa77f34ccee72833513af41e97b351f7c8dd8958
mahoneybj <benmahoney10@gmail.com>
2024-04-23 12:42:30 +1200

    Removed unused controllers

--- controllers/about.js
+++
@@ -1,8 +0,0 @@
-// Create a GET route
-const about = (req, res) => { 
-    res.send({ID: '123456', FirstName: 'Ben' , LastName: 'Mahoney', Fav: 'Embedded'});
-
-  };
-  
-  // Export the get function
-  export { about };
