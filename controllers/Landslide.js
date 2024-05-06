58a4bbaf58ab7b73aa990f986ed6ea3b339e32fb
mahoneybj <benmahoney10@gmail.com>
2024-05-06 14:41:21 +1200

    Added pagination to landslide

--- controllers/Landslide.js
+++ controllers/Landslide.js
@@ -38,7 +38,12 @@ const createLandslide = async (req, res) => {
       const sortBy = req.query.sortBy || "id" || "smallest" || "largest" || "region" || "number" || "earthquake_id";
       const sortOrder = req.query.sortOrder === "desc" ? "desc" : "asc";
   
+      const amount = req.query.amount || paginationDefault.amount;
+      const page = req.query.page || paginationDefault.page;
+  
       const query = {
+        take: Number(amount),
+        skip: (Number(page) - 1) * Number(amount),
         orderBy: {
           [sortBy]: sortOrder,
         },
