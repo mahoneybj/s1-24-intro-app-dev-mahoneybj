cc5ed2b9ca55722c09f97b0b4412b2a97a96c169
mahoneybj <benmahoney10@gmail.com>
2024-05-06 14:41:00 +1200

    Added pagination to eewinfo

--- controllers/eewinfo.js
+++ controllers/eewinfo.js
@@ -38,7 +38,12 @@ const createEEWInfo = async (req, res) => {
       const sortBy = req.query.sortBy || "id" || "alert_triggered" || "date" || "region" || "duration" || "accuracy" || "earthquake_id";
       const sortOrder = req.query.sortOrder === "desc" ? "desc" : "asc";
   
+      const amount = req.query.amount || paginationDefault.amount;
+      const page = req.query.page || paginationDefault.page;
+  
       const query = {
+        take: Number(amount),
+        skip: (Number(page) - 1) * Number(amount),
         orderBy: {
           [sortBy]: sortOrder,
         },
