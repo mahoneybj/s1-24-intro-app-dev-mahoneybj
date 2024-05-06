181e859205e5224186a01d7e84d062d62f6bdfa6
mahoneybj <benmahoney10@gmail.com>
2024-05-06 14:41:36 +1200

    Added pagination to sensorinfo

--- controllers/sensorinfo.js
+++ controllers/sensorinfo.js
@@ -38,7 +38,12 @@ const createSensorinfo = async (req, res) => {
       const sortBy = req.query.sortBy || "id" || "location" || "region" || "sensor_type" || "activate";
       const sortOrder = req.query.sortOrder === "desc" ? "desc" : "asc";
   
+      const amount = req.query.amount || paginationDefault.amount;
+      const page = req.query.page || paginationDefault.page;
+  
       const query = {
+        take: Number(amount),
+        skip: (Number(page) - 1) * Number(amount),
         orderBy: {
           [sortBy]: sortOrder,
         },
