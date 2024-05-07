a5fe8be98f58aea858e2d9137754530770c652bc
mahoneybj <benmahoney10@gmail.com>
2024-05-08 00:02:35 +1200

    Added index endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 // Create a GET route
 const get = (req, res) => { 
-    res.send('Hello, World!');
+    res.send('Endpoints:\n/api/building/\n/api/earthquake/\napi/eewinfo/\n/api/landslide\n/api/sensorinfo/\n/api/tsunami/');
 
   };
   
