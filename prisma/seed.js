c2c0b8d82f603c43267e777652f7f51727ad4d13
mahoneybj <benmahoney10@gmail.com>
2024-05-08 16:20:37 +1200

    Added data to seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -11,7 +11,41 @@ const main = async () => {
         duration: 30, 
         intensity: 5, 
         fault_line: "Alpine fault", 
-        after_shock_id: null, 
+        after_shock_id: null,
+        tsunami: {
+          create: {
+            region: "Otago",
+            size: 5, 
+            duration: 600, 
+            date: new Date(),
+          },
+          buildingDamage: {
+            create: {
+              houses_damaged: 25,
+              houses_destroyed: 6,
+              commerical_damaged: 54,
+              commerical_destroyed: 2,
+              cost: 3.6
+            }
+          },
+          EEWInfo: {
+            create: {
+              alert_triggered: true,
+              date: new Date(),
+              region: "Otago",
+              duration: 56,
+              accuracy: 67.3
+            }
+          },
+          Landslide: {
+            create: {
+              smallest: 2,
+              largest: 54,
+              region: "Otago",
+              number: 32
+            }
+          }
+        },
       },
     });
   } catch (err) {
