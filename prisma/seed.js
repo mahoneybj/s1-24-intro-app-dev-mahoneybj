d2272c858d2bd25ad187f8acafea2f68fc20fd4b
mahoneybj <benmahoney10@gmail.com>
2024-05-08 14:30:55 +1200

    Added data to earthquake seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,9 +3,17 @@ const prisma = new PrismaClient();
 
 const main = async () => {
   try {
-
-
-    
+    await prisma.earthquake.create({
+      data: {
+        date: new Date(), 
+        magnitude: 5.2, 
+        depth: 10.5, 
+        duration: 30, 
+        intensity: 5, 
+        fault_line: "Alpine fault", 
+        after_shock_id: null, 
+      },
+    });
   } catch (err) {
     console.error(err);
   } finally {
