8915fd4c17ac2bcacf041c17f1f33e619d7e9617
Ben Mahoney <benmahoney10@gmail.com>
2024-05-19 23:55:34 +1200

    Added jsdocs to /routes/landslide.js

--- routes/landslide.js
+++ routes/landslide.js
@@ -1,3 +1,8 @@
+/**
+ * @file Defines routes for Landslide related API endpoints. Middleware is used for request body validation on create and update endpoints.
+ * @author Ben Mahoney
+ */
+
 import express from "express";
 import { validatePostLandslide } from "../middleware/validation.js";
 
