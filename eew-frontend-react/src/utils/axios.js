4bd2302a53ac9ab0d255d94720894803ffb15a24
Ben <benmahoney10@gmail.com>
2024-05-23 23:16:05 +1200

    Fixed wrong axios variable name

--- eew-frontend-react/src/utils/axios.js
+++ eew-frontend-react/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Create an axios instance
-const studentManagementSystemInstance = axios.create({
+const earthquakeEarlyWarningSystemInstance = axios.create({
   baseURL: "https://s1-24-intro-app-dev-mahoneybj.onrender.com/api", // Replace with your own API URL
   timeout: 100000, // 100 seconds. Increase if requests are timing out
 });
