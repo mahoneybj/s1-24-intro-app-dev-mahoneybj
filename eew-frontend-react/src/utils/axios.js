04d51bc823445f43b49b2df90d0b33ac778e5a8b
Ben <benmahoney10@gmail.com>
2024-05-23 23:01:15 +1200

    Created and added axios code

---
+++ eew-frontend-react/src/utils/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// Create an axios instance
+const studentManagementSystemInstance = axios.create({
+  baseURL: "https://s1-24-intro-app-dev-mahoneybj.onrender.com/api", // Replace with your own API URL
+  timeout: 100000, // 100 seconds. Increase if requests are timing out
+});
+
+export { earthquakeEarlyWarningSystemInstance };
